a533ed0fae1a615ed5f129d9c9f1c08eb5c3983f
Peng-Jen <timisprettygood@gmail.com>
2023-12-26 01:39:19 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,30 +17,16 @@ for family in ["australian", "wisconsin", "votes", "nursery", "careval"]:
     dh.show_details(X, y)
 
     # feature_selection
-    lambda_ = [0.55, 0.85]
-    res = feature_selection(X, y, lambda_=lambda_)
-    
-    # output
-    prettifier(res)
+    lambda_list = [
+        # [lambda(-1), lambda(1)]
+        [0.75, 0.85], [0.8, 0.85], [0.65, 0.9]
+    ]
+    for lambda_ in lambda_list:
+        res = feature_selection(X, y, lambda_=lambda_)
+        
+        # output
+        prettifier(res)
 
-    # write file
-    with open(f"{path[:-12]}/results/{family}_{lambda_[0]}_{lambda_[1]}.txt", mode="w") as f:
-        print(res, file=f)
-
-
-# dataset = "australian"
-# lambda_ = [0.6, 0.9] # [\lambda_{-1}, \lambda_1]
-# print(f"Using lambda = {lambda_}")
-# dh = DataHolder()
-# X, y = dh.get(dataset)  # not yet 我通靈不到他怎麼算的
-# scaler = RobustScaler()
-# X[X.columns] = scaler.fit_transform(X[X.columns])
-
-# dh.show_details(X, y)
-# y = y.replace(0, -1)
-# res = feature_selection(X, y, lambda_=lambda_)
-
-
-
-# with open(f"../results/{dataset}_{lambda_[0]},{lambda_[1]}.pickle","wb") as f:
-#     pickle.dump(res, f)
+        # write file
+        with open(f"{path[:-12]}/results/{family}_{lambda_[0]}_{lambda_[1]}.txt", mode="w") as f:
+            print(res, file=f)
